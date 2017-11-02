27b43343c114853236cb4d191ac719866bb20399
Peter Nehrer <pnehrer@eclipticalsoftware.com>
2017-11-02 18:15:58 -0400

    Verify cmd-line args before loading additional modules.

--- odoo-search/lib/odoo-search.js
+++ odoo-search/lib/odoo-search.js
@@ -1,11 +1,6 @@
 #!/usr/bin/env node
 
 const _ = require('lodash');
-const Promise = require('bluebird');
-const xmlrpc = require('xmlrpc');
-const url = require('url');
-
-Promise.promisifyAll(require('xmlrpc/lib/client').prototype);
 
 const program = require('commander');
 const pkg = require('../package.json');
@@ -16,8 +11,8 @@ program
     .option('-u, --username <USERNAME>', 'Odoo username.')
     .option('-p, --password <PASSWORD>', 'Password for the given username.')
     .option('-o, --object <OBJECT>', 'Object to query; e.g., "res.partner".')
-    .option('-q, --query <DOMAINS>', 'JSON-encoded list of Odoo domains to apply. If unspecified, all records are returned.', (val) => val.split(','))
-    .option('-f, --fields <FIELD1[,...]>', 'Comma-separated list of fields to return. If unspecified, all fields are returned.', (val) => val.split(','))
+    .option('-q, --query <DOMAINS>', 'JSON-encoded list of Odoo domains to apply. If unspecified, all records are returned.', (val) => val.split(/\s*,+\s*/))
+    .option('-f, --fields <FIELD1[,...]>', 'Comma-separated list of fields to return. If unspecified, all fields are returned.', (val) => val.split(/\s*,+\s*/))
     .version(pkg.version)
     .parse(process.argv);
 
@@ -30,6 +25,12 @@ if (typeof program.object === 'undefined') {
     console.error('No object specified.');
     program.help();
 }
+
+const Promise = require('bluebird');
+const xmlrpc = require('xmlrpc');
+const url = require('url');
+
+Promise.promisifyAll(require('xmlrpc/lib/client').prototype);
 
 const config = _.pick(program, [ 'url', 'database', 'username', 'password', 'object', 'query', 'fields' ]);
 
@@ -45,10 +46,10 @@ commonSvc.methodCallAsync('authenticate', [ config.db, config.username, config.p
     if (config.query) {
         try {
             query = JSON.parse(config.query);
-            } catch(err) {
-                console.error('Error parsing argument `query\': ' + err.message);
-                program.help();
-            }
+        } catch(err) {
+            console.error('Error parsing argument `query\': ' + err.message);
+            program.help();
+        }
     }
 
     const domains = query ? _.castArray(query) : [ ];
